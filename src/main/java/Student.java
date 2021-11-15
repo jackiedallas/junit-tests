a87ae4a48cd4ed3615d7577065f916854297cea5
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-11-15 14:02:42 -0600

    added stuent class methods and constructor

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,2 +1,42 @@
+import java.util.ArrayList;
+
 public class Student {
+
+    // properties
+    public long id;
+    public String name;
+    public ArrayList<Integer> grades;
+
+    //constructor
+    public Student(String name, long id) {
+        this.name = name;
+        this.id = id;
+        this.grades = new ArrayList<>();
+    }
+
+    //methods
+    public long getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void addGrade(int grade) {
+        grades.add(grade);
+    }
+
+    public ArrayList<Integer> getGrades() {
+        return this.grades;
+    }
+
+    public double getGradeAverage() {
+        int sum = 0;
+        for (Integer grade : this.grades) {
+            sum += grade;
+        }
+        return (double) sum / this.grades.size();
+    }
+
 }
